85bf70704edec2832a5d6b101fe6e61f143fb32f
bhavra <bhavaniakumalla01@gmail.com>
2022-11-17 11:56:58 -0800

    Practice questions

--- Midterm/Question2/src/com/company/findParent.java
+++ Midterm/Question2/src/com/company/findParent.java
@@ -1,6 +1,13 @@
 package com.company;
 
 public class findParent {
-
-
+    public static void populateParent(Node node){
+        if(node == null)
+            return;
+        if(node.left!=null || node.right!=null) {
+            System.out.println(node.data);
+            populateParent(node.left);
+            populateParent(node.right);
+        }
+    }
 }
